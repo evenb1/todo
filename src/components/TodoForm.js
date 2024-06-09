26ffee5f94a8e69286d21bf335f636ce29cd8040
Even <even.berhane2022@gmail.com>
2024-06-09 22:05:17 +0300

    onChange attribute

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react'
 
 export const TodoForm = () => {
+    const [value, setValue] = useState("")
+
+
   return (
 <form className='TodoForm'>
         <input type='text' className='todo-input' placeholder='Whats the task today?'></input>
-        <button type='submit' className='todo-btn'>Add</button>
+        <button type='submit' className='todo-btn' 
+        onChange={(e) => setValue(e.target.value)}>Add</button>
     </form>
       )
 }
