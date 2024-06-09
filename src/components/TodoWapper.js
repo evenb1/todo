37b423f550b51fea86faa09808d70532b8e0cd7d
Even <even.berhane2022@gmail.com>
2024-06-09 09:39:03 +0300

    to do wrapper component

---
+++ src/components/TodoWapper.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TodoForm } from './TodoForm'
+
+export const TodoWapper = () => {
+  return (
+    <div className='TodoWrapper'>
+        <TodoForm/>
+    </div>
+
+)
+}
