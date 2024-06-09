a647682604edbbcbcf9d6c6e47ef2cba7863aa94
Even <even.berhane2022@gmail.com>
2024-06-09 22:06:40 +0300

    handleSubmit event listener

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,10 +2,12 @@ import React, {useState} from 'react'
 
 export const TodoForm = () => {
     const [value, setValue] = useState("")
-
+    const handleSubmit = e =>{
+        
+    }
 
   return (
-<form className='TodoForm'>
+<form className='TodoForm' onSubmit={handleSubmit}>
         <input type='text' className='todo-input' placeholder='Whats the task today?'></input>
         <button type='submit' className='todo-btn' 
         onChange={(e) => setValue(e.target.value)}>Add</button>
